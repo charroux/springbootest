cf021bdae6cac348e4b2f976b1244071f65d2cde
charroux <benoit.charroux@gmail.com>
2023-03-09 15:51:03 +0100

    first commit

---
+++ src/test/java/com/example/springbootest/SpringbootestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.springbootest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootestApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
